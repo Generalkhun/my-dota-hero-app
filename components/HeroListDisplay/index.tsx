011f4b02b4611f62407dd8c73b648f2dde159da0
Generalkhun <khun2551@hotmail.com>
2021-09-10 01:38:48 +0700

    Change hover color

--- components/HeroListDisplay/index.tsx
+++ components/HeroListDisplay/index.tsx
@@ -1,10 +1,6 @@
 import { Avatar, Theme, makeStyles, createStyles, Paper, ListItem, Grid } from '@material-ui/core';
 import React from 'react'
 import { map } from 'lodash'
-import { FixedSizeList } from 'react-window'
-import VirtualizedList from './components/VertualizedList';
-import { KeyboardReturnTwoTone } from '@material-ui/icons';
-
 interface Props {
     resHeroStatsData: Array<any>
 }
@@ -18,6 +14,7 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
         marginLeft: 20,
         height: '100%',
         width: '80%',
+        backgroundColor: '#2C2E43'
     },
     listItemDisplay: {
         backgroundColor: 'black'
@@ -28,17 +25,6 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 const HeroListDisplay = (props: Props) => {
     const { resHeroStatsData } = props
     const classes = useStyles();
-    // const resHeroStatsDataCol1 = resHeroStatsData.slice(0, 11)
-    // const resHeroStatsDataCol2 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol3 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol4 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol5 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol6 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol7 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol8 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol9 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol10 = resHeroStatsData.slice(11, 22)
-    // const resHeroStatsDataCol11 = resHeroStatsData.slice(11, 22)
 
     return (
         <Grid container>
