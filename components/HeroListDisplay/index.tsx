30b390cd28cb381efe942c9f120149df1ee66ebf
Generalkhun <khun2551@hotmail.com>
2021-09-10 01:34:51 +0700

    Adjust backgound

--- components/HeroListDisplay/index.tsx
+++ components/HeroListDisplay/index.tsx
@@ -1,9 +1,6 @@
 import { Avatar, Theme, makeStyles, createStyles, Paper, ListItem, Grid } from '@material-ui/core';
 import React from 'react'
 import { map } from 'lodash'
-import { FixedSizeList } from 'react-window'
-import VirtualizedList from './components/VertualizedList';
-import { KeyboardReturnTwoTone } from '@material-ui/icons';
 
 interface Props {
     resHeroStatsData: Array<any>
@@ -11,16 +8,19 @@ interface Props {
 const useStyles = makeStyles((theme: Theme) => createStyles({
     largeAvatar: {
         width: theme.spacing(7),
-        height: theme.spacing(7),
+        height: theme.spacing(7)
     },
+
     avatarsWrapper: {
         marginTop: 20,
         marginLeft: 20,
         height: '100%',
         width: '80%',
+        backgroundColor: 'black'
+
     },
     listItemDisplay: {
-        backgroundColor: 'black'
+        backgroundColor: 'black',
     }
 }));
 
@@ -46,8 +46,9 @@ const HeroListDisplay = (props: Props) => {
                 return (
                     <Grid item xs={1}>
                         <Paper className={classes.avatarsWrapper}>
-                            {map(resHeroStatsData.slice(0+(colNum*11), 11+(colNum*11)), function (heroStatus, index) {
+                            {map(resHeroStatsData.slice(0 + (colNum * 11), 11 + (colNum * 11)), function (heroStatus, index) {
                                 return (
+
                                     <ListItem className={classes.listItemDisplay} button key={index}>
                                         <Avatar alt="Hero" src={`http://cdn.dota2.com/${heroStatus.img}`} className={classes.largeAvatar} />
                                     </ListItem>
