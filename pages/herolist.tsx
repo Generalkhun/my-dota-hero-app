1d4e6efccb53cff428c4f8d4f4c8fd12296465ac
Generalkhun <khun2551@hotmail.com>
2021-09-09 17:33:43 +0700

    Add fetching data

--- pages/herolist.tsx
+++ pages/herolist.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 
 interface Props {
-    
+
 }
 
 const herolist = (props: Props) => {
+    console.log(props);
+
     return (
         <div>
-           herolist 
+            {JSON.stringify(props.data)}
         </div>
     )
 }
+
+export async function getStaticProps(context) {
+    const resHero = await fetch(`https://api.opendota.com/api/heroes`)
+    const resHeroStats = await fetch(`https://api.opendota.com/api/heroStats`)
+    // http://cdn.dota2.com/apps/dota2/images/heroes/crystal_maiden_full.png
+
+    const resHeroData = await resHero.json()
+    const resHeroStatsData = await resHeroStats.json()
+    if (!resHeroData || !resHeroStatsData) {
+        return {
+            notFound: true,
+        }
+    }
+    return {
+        props: { data: { resHeroData, resHeroStatsData } }, // will be passed to the page component as props
+    }
+}
+
 
 export default herolist
