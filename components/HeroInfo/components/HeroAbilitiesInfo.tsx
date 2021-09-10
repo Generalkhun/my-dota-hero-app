267258e848efd4142591fa8705923ad26dd4e814
Generalkhun <khun2551@hotmail.com>
2021-09-10 21:03:03 +0700

    Add other attributes

--- components/HeroInfo/components/HeroAbilitiesInfo.tsx
+++ components/HeroInfo/components/HeroAbilitiesInfo.tsx
@@ -1,8 +1,5 @@
 import { Card, Divider, Grid, IconButton, makeStyles, Paper, Typography } from '@material-ui/core'
 import React from 'react'
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
-import PanoramaFishEyeIcon from '@material-ui/icons/PanoramaFishEye';
-
 interface heroAbilitiesDataType {
     base_agi: number,
     base_armor: number,
@@ -34,7 +31,6 @@ const useStyles = makeStyles({
     },
     herosAbilitiesInfoWrapper: {
         marginLeft: 20,
-        marginTop: 20,
         width: '100%',
         height: '48vh',
         backgroundColor: '#444444'
@@ -47,9 +43,6 @@ const useStyles = makeStyles({
     titleWrapper: {
         backgroundColor: '#1E212D',
         width: 'auto',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: "center"
     },
     contentWrapper: {
         marginTop: 10,
@@ -58,62 +51,93 @@ const useStyles = makeStyles({
     },
 });
 
-const HeroAbilitiesInfo = ({heroAbilitiesData}: Props) => {
+const HeroAbilitiesInfo = ({ heroAbilitiesData }: Props) => {
     const classes = useStyles()
     return (
         <Paper className={classes.herosAbilitiesInfoWrapper}>
 
-            <Grid container style={{ marginLeft: 10, marginTop: 10 }}>
-                <Grid item xs={12} md={6}>
+            <Grid container style={{ marginLeft: 10 }}>
+                <Grid item xs={10} md={6} style={{ marginTop: 10, marginLeft: 20 }}>
                     <Card className={classes.titleWrapper} variant="outlined">
-                        <Typography variant='h6' style={{ fontFamily:'Cinzel',color: 'white', marginLeft: 10 }}>△ BASE ATTR</Typography>
+                        <Typography variant='h6' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>△ BASE ATTRIBUTES</Typography>
                     </Card>
                     <Card className={classes.contentWrapper} variant="outlined">
                         <Grid container>
                             <Grid item md={6}>
-                            {/* base_agi: number,
-    base_armor: number,
-    base_attack_max: number,
-    base_attack_min: number,
-    base_health: number,
-    base_health_regen: number,
-    base_int: number
-    base_mana: number,
-    base_mana_regen: number,
-    base_mr: number,
-    base_str: number */}
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>AGI</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>STR</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>INT</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>AGI</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>STR</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>INT</Typography>
 
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>ARMOR</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>ATTACK-MAX</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>ATTACK-MIN</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>ARMOR</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>ATTACK-MAX</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>ATTACK-MIN</Typography>
 
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>HEALTH</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>MANA</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>MANA-REGEN</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>MR</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>HEALTH</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>MANA</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>MANA-REGEN</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>MR</Typography>
                             </Grid>
                             <Grid item md={6}>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_agi}</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_agi}</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_agi}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_agi}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_agi}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_agi}</Typography>
 
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_armor}</Typography>
-                                <Typography variant='body1' style={{ fontFamily:'Cinzel',color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_attack_max}</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_attack_min}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_armor}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_attack_max}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_attack_min}</Typography>
 
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_health}</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_mana}</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_mana_regen}</Typography>
-                                <Typography variant='body1' style={{fontFamily:'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_mr}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_health}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_mana}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_mana_regen}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.base_mr}</Typography>
                             </Grid>
                         </Grid>
 
                     </Card>
                 </Grid>
-                <Grid item xs={12} md={6}>special</Grid>
+                <Grid item xs={10} md={5} style={{ marginTop: 10, marginLeft: 20 }}>
+                    <Card className={classes.titleWrapper} variant="outlined">
+                        <Typography variant='h6' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>✩ OTHER ATTRIBUTES</Typography>
+                    </Card>
+
+                    {/* agi_gain: number,
+    str_gain: number,
+    int_gain: number,
+
+    attack_rate: number,
+    attack_type: string,
+    cm_enabled: boolean,
+    move_speed: number, */}
+                    <Card className={classes.contentWrapper} variant="outlined">
+                        <Grid container>
+                            <Grid item md={6}>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>AGI-GAIN</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>STR-GAIN</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>INT-GAIN</Typography>
+
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>ATTACK-RATE</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>ATTACK-TYPE</Typography>
+
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>CM-ENABLED</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>MOVE-SPEED</Typography>
+                            </Grid>
+                            <Grid item md={6}>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.agi_gain}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.str_gain}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.int_gain}</Typography>
+
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.attack_rate}</Typography>
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.attack_type}</Typography>
+
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.cm_enabled?'✔':'✘'}</Typography>
+
+                                <Typography variant='body1' style={{ fontFamily: 'Cinzel', color: 'white', marginLeft: 10 }}>{heroAbilitiesData.move_speed}</Typography>
+                            </Grid>
+                        </Grid>
+
+                    </Card>
+
+                </Grid>
             </Grid>
         </Paper>
     )
