3c10c6873ec2dca1543ca8e353be276446114fda
Generalkhun <khun2551@hotmail.com>
2021-09-12 13:58:17 +0700

    Add icon on choosing attr

--- components/DisplaySetting/components/FilterBox/components/BaseAttrFiltering.tsx
+++ components/DisplaySetting/components/FilterBox/components/BaseAttrFiltering.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FolderIcon from '@material-ui/icons/Folder';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
 
 const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    height:40,
-    borderRadius:4
-  },
+    root: {
+        width: '100%',
+        height: 40,
+        borderRadius: 4,
+        backgroundColor: 'black'
+    },
+    attrChooseLabel: {
+        color:'white',
+    }
 });
 
 
@@ -20,20 +21,21 @@ interface Props {
     onFilterHero: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function LabelBottomNavigation({onFilterHero}) {
-  const classes = useStyles();
-  const [value, setValue] = React.useState('recents');
+export default function LabelBottomNavigation({ onFilterHero }) {
+    const classes = useStyles();
+    const [value, setValue] = React.useState('recents');
 
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
-    setValue(newValue);
-  };
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setValue(newValue);
+    };
 
-  return (
-    <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
-      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    </BottomNavigation>
-  );
+    return (
+        <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+            <BottomNavigationAction label="ALL" value="all" style={{ color: 'white' }} icon={<SupervisedUserCircleIcon style={{ color: 'white' }} />} />
+            <BottomNavigationAction label="INT" value="int" style={{ color: 'white'}} icon={<img height='25' src='https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png'/>} />
+            <BottomNavigationAction label="STR" value="str" style={{ color: 'white' }} icon={<img height='25' src='https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png'/>} />
+            <BottomNavigationAction label="AGI" value="agi" style={{ color: 'white' }} icon={<img height='25' src='https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png'/>} />
+            
+        </BottomNavigation>
+    );
 }
