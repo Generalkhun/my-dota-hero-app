d2c0f4f51c4ed85874ff4cbae860b5609b6c7d7e
Generalkhun <khun2551@hotmail.com>
2021-09-12 11:47:09 +0700

    FIx search box styling

--- components/DisplaySetting/components/SearchBox.tsx
+++ components/DisplaySetting/components/SearchBox.tsx
@@ -37,6 +37,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     inputRoot: {
       color: 'inherit',
+      width: '100%'
     },
     inputInput: {
       padding: theme.spacing(1, 1, 1, 0),
@@ -45,7 +46,7 @@ const useStyles = makeStyles((theme: Theme) =>
       color:'white',
       transition: theme.transitions.create('width'),
       width: '100%',
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up('xs')]: {
         width: '37ch',
         '&:focus': {
           width: '37ch',
