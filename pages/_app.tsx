1bb6589fdab1dbfd947d971504726e92e6e57304
Generalkhun <khun2551@hotmail.com>
2021-09-13 16:46:32 +0700

    Add basic load spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,25 @@ import React, { useState } from 'react'
 import Layouts from '../components/Layouts'
 import '../styles/globals.css'
 import { FavoriteHerosProvider } from '../contextProviders/FavoriteHerosProvider'
+import dynamic from 'next/dynamic'
+
+
+// global styles are required to be added to `_app.js` per Next.js requirements.
+import "nprogress/nprogress.css";
+
+const TopProgressBar = dynamic(
+  () => {
+    return import("../components/TopProgreeBar");
+  },
+  { ssr: false },
+);
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <FavoriteHerosProvider>
       <Layouts>
+        <TopProgressBar />
         <Component {...pageProps} />
       </Layouts>
     </FavoriteHerosProvider>
